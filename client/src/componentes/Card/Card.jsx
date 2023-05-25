5222000ccda782259465954eb46d3be79696c6c4
FidelFuentes <fidelfuentes.fullstack@gmail.com>
2023-05-25 13:59:04 +1200

    deploy3

--- client/src/componentes/Card/Card.jsx
+++ client/src/componentes/Card/Card.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 import style from './Card.module.css'
 import { useDispatch } from 'react-redux';
 import { deletePokemon } from '../../redux/actions'; 
-//que debo hacer aca ==>
+//que debo hacer aca ==> paso 4 completo
 
 const Card = ({ id, name, image, types, created }) => {
     const dispatch = useDispatch();
